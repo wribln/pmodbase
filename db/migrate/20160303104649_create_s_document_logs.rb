40ca4f941a9bcce651a2acee38576ca707af43eb
Wilfried Roemer <wilfried.roemer@web.de>
2016-03-23 01:31:15 +0100

    after release fix 0.3 - s_document_log had date with limit

--- db/migrate/20160303104649_create_s_document_logs.rb
+++ db/migrate/20160303104649_create_s_document_logs.rb
@@ -2,6 +2,7 @@ class CreateSDocumentLogs < ActiveRecord::Migration
   def change
     create_table :s_document_logs do |t|
       t.belongs_to :group,        null: false
+      t.belongs_to :account,      null: false 
       t.string :receiver_group,   limit: MAX_LENGTH_OF_CODE
       t.string :function_code,    limit: MAX_LENGTH_OF_CODE
       t.string :service_code,     limit: MAX_LENGTH_OF_CODE
@@ -10,8 +11,7 @@ class CreateSDocumentLogs < ActiveRecord::Migration
       t.string :phase_code,       limit: MAX_LENGTH_OF_CODE
       t.string :dcc_code,         limit: MAX_LENGTH_OF_CODE
       t.string :revision_code,    limit: MAX_LENGTH_OF_CODE
-      t.date :author_date,        limit: MAX_LENGTH_OF_CODE
-      t.belongs_to :account,      null: false 
+      t.date :author_date
       t.string :title,            limit: MAX_LENGTH_OF_TITLE
       t.string :siemens_doc_id,   limit: MAX_LENGTH_OF_DOC_ID_S
 
