c7b0bbc2e4f9a38d2e3cc881477778a5a27088b8
Wilfried Roemer <wilfried.roemer@web.de>
2016-08-19 00:21:34 +0200

    fix foreign key for cfr_relations

--- db/migrate/20160809114810_create_cfr_relations.rb
+++ db/migrate/20160809114810_create_cfr_relations.rb
@@ -1,10 +1,12 @@
 class CreateCfrRelations < ActiveRecord::Migration
   def change
     create_table :cfr_relations do |t|
-      t.belongs_to :src_record,       index: true, foreign_key: true
-      t.belongs_to :dst_record,       index: true, foreign_key: true
+      t.belongs_to :src_record,       index: true
+      t.belongs_to :dst_record,       index: true
       t.belongs_to :cfr_relationship, index: true, foreign_key: true
       t.timestamps null: false
     end
+  add_foreign_key :cfr_relations, :cfr_records, column: :src_record_id
+  add_foreign_key :cfr_relations, :cfr_records, column: :dst_record_id
   end
 end
