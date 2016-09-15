5d5635cd61c03f587a12fae1c6272d79d24273c3
Wilfried Roemer <wilfried.roemer@web.de>
2016-09-15 23:04:24 +0200

    glossary moved after cfr

---
+++ db/migrate/20160809114811_create_glossary_items.rb
@@ -0,0 +1,13 @@
+class CreateGlossaryItems < ActiveRecord::Migration
+  def change
+    create_table :glossary_items do |t|
+      t.string      :term,     limit: MAX_LENGTH_OF_TERM,         null: false, index: true
+      t.string      :code,     limit: MAX_LENGTH_OF_CODE,                      index: true
+      t.text        :description
+      t.belongs_to  :cfr_record, foreign_key: :true
+
+      t.timestamps null: false
+    end
+  end
+end
+
