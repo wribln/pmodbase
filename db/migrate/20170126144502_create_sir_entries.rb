fa59f76c130355e2df6151606c40493d085dd282
Wilfried Roemer <wilfried.roemer@web.de>
2017-03-12 19:29:39 +0100

    0.2.1.1 foreign_key fix

--- db/migrate/20170126144502_create_sir_entries.rb
+++ db/migrate/20170126144502_create_sir_entries.rb
@@ -2,14 +2,16 @@ class CreateSirEntries < ActiveRecord::Migration
   def change
     create_table :sir_entries do |t|
       t.belongs_to  :sir_item,  index: true, foreign_key: true
-      t.belongs_to  :orig_group, null: false, foreign_key: :group_id
-      t.belongs_to  :resp_group, null: false, foreign_key: :group_id
+      t.belongs_to  :orig_group, null: false
+      t.belongs_to  :resp_group, null: false
       t.integer     :rec_type,   null: false
       t.date        :due_date
       t.text        :description
 
       t.timestamps  null: false
     end
+    add_foreign_key :sir_entries, :groups, column: :orig_group_id
+    add_foreign_key :sir_entries, :groups, column: :resp_group_id
     add_index :sir_entries, [ :sir_item_id, :created_at ],
               order: { sir_item_id: :asc, created_at: :asc },
               name: 'sir_entries_default_order'
